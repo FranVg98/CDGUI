73020007bbf17344a267c0b65532343f8e9caaec
FranVg98 <pape.valenzuela@hotmail.com>
2022-04-15 23:25:54 -0400

    Agregar archivos de proyecto.

---
+++ GUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace GUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----GUI-----");
+            Console.WriteLine("Escriba el rango (ejemplo 1,5): ");
+            string mensaje = Console.ReadLine();
+            Cliente cliente = new Cliente();
+
+            //mandar primer rango
+            //ip
+            cliente.Connect("", mensaje, "8888");
+
+            Thread.Sleep(1000);
+
+            cliente.RecibirMensaje();
+
+            cliente.CerrarCliente();
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
